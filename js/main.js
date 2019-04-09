35fad331f93d76900d2161680eb7120671d55a9c
Tobiasz Maj <tobiaszmaj1@gmail.com>
2019-04-09 21:30:43 +0200

    Adding Quotes

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Immediate invoked function expression
+
+(function () {
+    const quotes = [{
+            quote: "I am not in danger, I am the danger.",
+            author: "Walter White"
+        },
+        {
+            quote: "There is gold in the streets just waiting for someone to come and scoop it up.",
+            author: "Walter White"
+        },
+        {
+            quote: "Congratulations, you’ve just left your family a second-hand Subaru.",
+            author: "Saul Goodman"
+        },
+        {
+            quote: "Nah, come on man. Some straight like you, giant stick up his ass at like what, sixty, he’s just gonna break bad?",
+            author: "Jesse Pinkman"
+        },
+        {
+            quote: "We’re done when I say we’re done.",
+            author: "Walter White"
+        },
+        {
+            quote: "You are not the guy. You’re not capable of being the guy. I had a guy, but now I don’t. You are not the guy.",
+            author: "Mike Ehrmantraut"
+        }
+    ];
+
+    const btn = document.getElementById('generate-btn');
+
+    btn.addEventListener('click', function () {
+        let random = Math.random();
+    });
+
+})();
